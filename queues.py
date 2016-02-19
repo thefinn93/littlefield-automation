c6c68f67ec1df528c2fab3d9e91441c969013ae9
Finn Herzfeld <finn@finn.io>
2016-02-19 01:58:02 -0800

    Add queues munin plugin

---
+++ queues.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import sys
+import os
+lib = os.getenv("LITTLEFIELD_PATH")
+if lib is not None:
+    sys.path.append(lib)
+from littlefield import Littlefield
+
+config = False
+if len(sys.argv) > 1:
+    if sys.argv[1] == "config":
+        config = True
+if config:
+    print("""graph_title Queue Size Per Station
+graph_info Shows the queue size per station
+graph_category bullshit
+graph_vlabel Kits
+""")
+
+littlefield = Littlefield(os.getenv("LITTLEFIELD_USER"), os.getenv("LITTLEFIELD_PW"))
+for station in range(1, 4):
+    if config:
+        print("station%s.label Station %s" % (station, station))
+    else:
+        size = littlefield.get_data("S%sQ" % station)['average'][-1]
+        print("station%s.value %s" % (station, size))
