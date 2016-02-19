b054ec40367bb2d1e92b3b6400dd97aec2dd180b
Finn Herzfeld <finn@finn.io>
2016-02-18 21:04:56 -0800

    Add munin plugin

---
+++ munin.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import sys
+import os
+lib = os.getenv("LITTLEFIELD_PATH")
+if lib is not None:
+    sys.path.append(lib)
+from littlefield import Littlefield
+
+config = False
+if len(sys.argv) > 1:
+    if sys.argv[1] == "config":
+        config = True
+if config:
+    print("""graph_title Littlefield Simulation Leaderboard
+graph_info Shows the position on the leadership board
+graph_category bullshit
+graph_vlabel Cash ($)
+""")
+
+littlefield = Littlefield(os.getenv("LITTLEFIELD_USER"), os.getenv("LITTLEFIELD_PW"))
+for team in littlefield.get_standings():
+    if config:
+        print("%s.label %s" % (team['name'], team['name']))
+    else:
+        print("%s.value %s" % (team['name'], team['cash']))
