dea1b03650807c562367848bcc6d002d8a5f7e45
Finn <finn@finn.io>
2016-02-23 19:12:08 -0800

    Fix high and low values to use the same scale

--- munin/utilitization.py
+++ munin/utilitization.py
@@ -24,11 +24,11 @@ littlefield = Littlefield(os.getenv("LITTLEFIELD_USER"), os.getenv("LITTLEFIELD_
 for station in range(1, 4):
     if config:
         print("station%s.label Station %s" % (station, station))
-        print("station%s.warning 0.1:0.8" % station)
+        print("station%s.warning 10:80" % station)
         print("station%s-3.label Station %s (3 day average)" % (station, station))
-        print("station%s-3.warning Station 0.1:0.8" % station)
+        print("station%s-3.warning Station 10:80" % station)
         print("station%s-10.label Station %s" % (station, station))
-        print("station%s-10.warning Station 0.1:0.8" % station)
+        print("station%s-10.warning Station 10:80" % station)
     else:
         util = littlefield.get_data("S%sUTIL" % station)['average']
         print("station%s.value %s" % (station, util[-1]*100))
