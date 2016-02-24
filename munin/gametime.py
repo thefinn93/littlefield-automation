7a9a0aaa9478c0e05a65587d8b40fecb30feeb31
Finn <finn@finn.io>
2016-02-23 19:20:12 -0800

    Display the current game time

---
+++ munin/gametime.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import sys
+import os
+lib = os.getenv("LITTLEFIELD_PATH")
+if lib is not None:
+    sys.path.append(lib)
+from littlefield import Littlefield
+
+config = False
+if len(sys.argv) > 1:
+    if sys.argv[1] == "config":
+        config = True
+if config:
+    print("graph_title Game Time")
+    print("graph_info Shows the number of game days that have passed")
+    print("graph_category littlefield")
+    print("graph_vlabel days")
+
+littlefield = Littlefield(os.getenv("LITTLEFIELD_USER"), os.getenv("LITTLEFIELD_PW"))
+if config:
+    print("days.label Game Days")
+else:
+    key = None
+    data = littlefield.get_status()
+    print("days.value %s" % status['day'])
