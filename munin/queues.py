3fd0b7edf83e9d80a42e2972b2b76af1ab2ca555
Finn <finn@finn.io>
2016-02-24 15:51:43 -0800

    Try to customize colors

--- munin/queues.py
+++ munin/queues.py
@@ -24,8 +24,11 @@ littlefield = Littlefield(os.getenv("LITTLEFIELD_USER"), os.getenv("LITTLEFIELD_
 for station in range(1, 4):
     if config:
         print("station%s.label Station %s" % (station, station))
+        print("station%s.colour COLOR%s" % (station, station-1))
         print("station%s-3.label Station %s (3 day average)" % (station, station))
+        print("station%s-3.colour COLOR%" % (station, station+8))
         print("station%s-10.label Station %s (10 day average)" % (station, station))
+        print("station%s-10.colour COLOR%s" % (station, station+16))
     else:
         size = littlefield.get_data("S%sQ" % station)['average']
         print("station%s.value %s" % (station, size[-1]))
